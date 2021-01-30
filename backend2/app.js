5819235d2768d7fd33d8302d73182b89de3b0be6
Liang Liu <lliu2@atb.com>
2021-01-30 04:32:10 -0800

    Add 3d viewer

--- backend2/app.js
+++ backend2/app.js
@@ -2,12 +2,13 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var usersRouter = require("./routes/users");
 var setupRouter = require("./routes/setup");
 require("./db/mongoose");
 var app = express();
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
---
+++ frontend/pages/setup/[id].js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import { withAuthUser } from 'next-firebase-auth'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import { makeStyles } from '@material-ui/core/styles'
+import { useRouter } from 'next/router'
+import Layout from '../../layout/layout'
+import { Viewer } from 'photo-sphere-viewer'
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}))
+
+const Setups = () => {
+  const classes = useStyles()
+  const router = useRouter()
+  const { id } = router.query
+  const [setup, setSetup] = useState()
+
+  useEffect(() => {
+    if (id)
+      fetch(process.env.NEXT_PUBLIC_BACKEND_URL + '/setup/' + id)
+        .then((response) => response.json())
+        .then((data) => setSetup(data))
+  }, [id])
+
+  const sphereElementRef = React.createRef()
+
+  useEffect(() => {
+    if (!setup) return
+    const shperePlayerInstance = new Viewer({
+      container: sphereElementRef.current,
+      panorama: `${process.env.NEXT_PUBLIC_BACKEND_URL}/setup/${id}/image`,
+    })
+
+    // unmount component instructions
+    return () => {
+      shperePlayerInstance.destroy()
+    }
+  }, [setup])
+
+  // eslint-disable-next-line react/jsx-no-undef
+  return (
+    <Layout title={'Setup'}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            {setup?.img && (
+              <div style={{ width: '100%', height: 400 }} ref={sphereElementRef} />
+            )}
+          </Paper>
+        </Grid>
+      </Grid>
+    </Layout>
+  )
+}
+
+export default withAuthUser({
+  // whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
+})(Setups)
