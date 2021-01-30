fac924d4db8d9870a48f356273973fed44a9dee0
Michael DeMarco <michaelfromyeg@gmail.com>
2021-01-30 06:36:02 -0800

    updating the footer

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
-const Footer = () => <h1>Footer</h1>
+const Footer = () => {
+  return (
+    <>
+      <p style={{ textAlign: 'center' }}>Made with ❤ by <Link href="https://github.com/stuffbyliang"><a>Liang</a></Link>, <Link href="https://github.com/michaelfromyeg"><a>Michael</a></Link>, and <Link href="https://github.com/haltaf19"><a>Hasan</a></Link> for BizHacks 2021.</p>
+      <p style={{ textAlign: 'center' }}>Showroom © 2021</p>
+    </>
+  )
+}
 
 export default Footer
