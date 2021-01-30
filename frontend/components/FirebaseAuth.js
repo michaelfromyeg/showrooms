32cde159d08d6619395fd7257e04421873a4defc
Michael DeMarco <michaelfromyeg@gmail.com>
2021-01-29 23:52:57 -0800

    added authentication

---
+++ frontend/components/FirebaseAuth.js
@@ -0,0 +1,44 @@
+  /* globals window */
+import React, { useEffect, useState } from 'react'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+// Note that next-firebase-auth inits Firebase for us,
+// so we don't need to.
+
+const firebaseAuthConfig = {
+  signInFlow: 'popup',
+  signInOptions: [
+    {
+      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    },
+  ],
+  signInSuccessUrl: '/',
+  credentialHelper: 'none',
+  callbacks: {
+    signInSuccessWithAuthResult: () =>
+      false,
+  },
+}
+
+const FirebaseAuth = () => {
+  const [renderAuth, setRenderAuth] = useState(false)
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setRenderAuth(true)
+    }
+  }, [])
+  return (
+    <div>
+      {renderAuth ? (
+        <StyledFirebaseAuth
+          uiConfig={firebaseAuthConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      ) : null}
+    </div>
+  )
+}
+
+export default FirebaseAuth
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,11 @@
+import React from 'react'
 import '../styles/globals.css'
+import initAuth from '../utils/initAuth'
+
+initAuth()
 
 function MyApp({ Component, pageProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
 
